a9a36c82940127c974099f432f3f326193c44cc2
simonq80 <simonq80@gmail.com>
2016-03-23 18:49:57 +0000

    init

---
+++ src/main/java/simon/SecureStorage/Main.java
@@ -0,0 +1,52 @@
+
+
+
+package simon.SecureStorage;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+import com.dropbox.core.v2.users.FullAccount;
+
+import java.util.List;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class Main {
+    private static final String ACCESS_TOKEN = "YVMLBbT9LfAAAAAAAAAACO8ieMv5s9sBa27LQ9CoA3NMoUF_s97z3DIyY_qWs-zu";
+
+    public static void main(String args[]) throws DbxException, IOException {
+        // Create Dropbox client
+        DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
+        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+
+        // Get current account info
+        FullAccount account = client.users().getCurrentAccount();
+        System.out.println(account.getName().getDisplayName());
+
+        // Get files and folder metadata from Dropbox root directory
+        ListFolderResult result = client.files().listFolder("");
+        while (true) {
+            for (Metadata metadata : result.getEntries()) {
+                System.out.println(metadata.getPathLower());
+            }
+
+            if (!result.getHasMore()) {
+                break;
+            }
+
+            result = client.files().listFolderContinue(result.getCursor());
+        }
+
+        // Upload "test.txt" to Dropbox
+        try (InputStream in = new FileInputStream("/home/simon/apache-maven-3.3.9-bin.zip")) {
+            FileMetadata metadata = client.files().uploadBuilder("/maven.txt")
+                .uploadAndFinish(in);
+        }
+    }
+}
